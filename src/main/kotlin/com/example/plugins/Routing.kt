3fcc5366133622b9fbb999ebe608a03dda40cf12
Natalia Dmitrieva <ndmitrieva@juliusspencer.co.nz>
2024-01-19 21:54:14 +1300

    + Added new http endpoint

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -3,11 +3,18 @@ package com.example.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.http.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/greetings") {
+            val text = "<h1>Hello From Ktor</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
         }
     }
 }
