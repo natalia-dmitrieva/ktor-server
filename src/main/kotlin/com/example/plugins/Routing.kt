2809be9f4f4a1de6d00d1bd166702f6f6938ebd5
Natalia Dmitrieva <idmitrievanm@gmail.com>
2024-01-23 22:07:12 +1300

    + Added static resources file, and invoked it within the application

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -4,9 +4,12 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.http.*
+import io.ktor.server.http.content.*
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/content", "mycontent")
+
         get("/") {
             call.respondText("Hello World!")
         }
